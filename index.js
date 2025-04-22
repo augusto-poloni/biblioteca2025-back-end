f2b07268fa6cace80e2b9332f0ff14c3e7d29992
Augusto Poloni <a_poloni@unoesc.edu.br>
2025-04-22 13:31:03 -0300

    Adições

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
 import express from "express";
-import { Sequelize, DataTypes } from "sequelize";
+import banco from "./banco.js";
+import editora from "./controller/EditoraController.js";
+import autor from "./controller/AutorController.js";
+import categoria from "./controller/CategoriaController.js";
+import livro from "./controller/LivroController.js";
+import usuario from "./controller/UsuarioController.js";
+import emprestimo from "./controller/EmprestimoController.js";
 
-//configuração com o banco
-const sequelize = new Sequelize('Biblioteca2025', 'postgres', '1002', {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres', 
-    define: {
-        timestamps: false,
-        freezeTableName: true
-    }
-  });
-  //mapeamento model editora
-  const User = sequelize.define(
-    'editora',
-    {
-      // Model attributes are defined here
-      ideditora: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      nomeeditora: {
-        type: DataTypes.STRING(60),
-        allowNull: false
-      },
-      cnpj:{
-        type: DataTypes.STRING(20),
-        allowNull: false
-      },
-      endereço:{
-        type: DataTypes.TEXT,
-        allowNull: true
-      },
-    },
-  );
-
-  
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+try {
+    await banco.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso.');
+} catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+}
 
 const app = express();
 app.use(express.json());
 
-app.get(`/teste`,(req, res)=>{
-    res.send(`Teste ok`);
-})
+app.get('/teste', (req, res) => {
+    res.send('Teste ok.');
+});
 
-app.listen(3000, ()=>{ console.log(`Servidor rodando.`)});
+//rotas crud da tabela editora
+app.get('/editora', editora.listar);
+app.get('/editora/:id', editora.selecionar);
+app.post('/editora', editora.inserir);
+app.put('/editora/:id', editora.alterar);
+app.delete('/editora/:id', editora.excluir);
+
+//rotas crud da tabela autor
+app.get('/autor', autor.listar);
+app.get('/autor/:id', autor.selecionar);
+app.post('/autor', autor.inserir);
+app.put('/autor/:id', autor.alterar);
+app.delete('/autor/:id', autor.excluir);
+
+//rotas crud da tabela categoria
+app.get('/categoria', categoria.listar);
+app.get('/categoria/:id', categoria.selecionar);
+app.post('/categoria', categoria.inserir);
+app.put('/categoria/:id', categoria.alterar);
+app.delete('/categoria/:id', categoria.excluir);
+
+//rotas crud da tabela livro
+app.get('/livro', livro.listar);
+app.get('/livro/:id', livro.selecionar);
+app.post('/livro', livro.inserir);
+app.put('/livro/:id', livro.alterar);
+app.delete('/livro/:id', livro.excluir);
+
+//rotas crud da tabela usuario
+app.get('/usuario', usuario.listar);
+app.get('/usuario/:id', usuario.selecionar);
+app.post('/usuario', usuario.inserir);
+app.put('/usuario/:id', usuario.alterar);
+app.delete('/usuario/:id', usuario.excluir);
+
+//rotas crud da tabela emprestimo
+app.get('/emprestimo', emprestimo.listar);
+app.get('/emprestimo/:id', emprestimo.selecionar);
+app.post('/emprestar', emprestimo.emprestar);
+app.put('/devolver/:id', emprestimo.devolver);
+
+app.listen(3000, () => { console.log(`Servidor rodando.`) });
